ce8764bc8bd7ec0233b9853ad1d1a570be30575d
PedroBonorin0 <pepebonorinobraga@gmail.com>
2021-09-23 20:21:01 -0300

    View Agenda

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path'); // CommomJS
+
+module.exports = {
+  mode: 'development',
+  entry: './frontend/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public', 'assets', 'js'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devtool: 'source-map'
+};  
